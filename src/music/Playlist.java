990f290dea627975ec6068c93f10934d1bbe1e92
Emir Pesereklü <emir.peserekli@gmail.com>
2025-04-18 23:22:21 +0330

    Playlist class is set

---
+++ src/music/Playlist.java
@@ -0,0 +1,63 @@
+package music;
+
+import behavior.User;
+import exceptions.InvalidOperationException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Playlist {
+    String title;
+    User owner;
+
+    ArrayList<Music> playlist;
+
+    public Playlist(String title, User owner) {
+        this.title = title;
+        this.owner = owner;
+        this.playlist = new ArrayList<>();
+    }
+
+    void editTitle(String newTitle, String password) {
+        if (owner.password.equals(password)) {
+            this.title = newTitle;
+        } throw new InvalidOperationException("Unauthorized access to edit playlist");
+    }
+    void addMusic(Music music, String password) {
+        if (owner.password.equals(password)) {
+            if (playlist.contains(music)) {
+                throw new InvalidOperationException("This track already exists in your playlist");
+            }
+            playlist.add(music);
+        } throw new InvalidOperationException("Unauthorized access to edit playlist");
+    }
+    void removeMusic(Music music, String password) {
+        if (owner.password.equals(password)) {
+            if (!(playlist.contains(music))) {
+                throw new InvalidOperationException("This track does not exist in your playlist");
+            }
+            playlist.remove(music);
+        } throw new InvalidOperationException("Unauthorized access to edit playlist");
+    }
+    Music searchInPlaylist(Music music) {
+        for (Music m : playlist) {
+            if (m.equals(music)) {
+                return music;
+            }
+        }
+        throw new InvalidOperationException("This music doesn't exist in your playlist");
+    }
+    void playPlaylist() {
+        for (Music music : playlist) {
+            music.play();
+        }
+    }
+    void shuffle() {
+        ArrayList<Music> shuffled = new ArrayList<>(playlist);
+        Collections.shuffle(shuffled);
+
+        for (Music music : shuffled) {
+            music.play();
+        }
+    }
+}
