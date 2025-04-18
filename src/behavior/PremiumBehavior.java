f783ac2022d475623e0d6a426befed6a9cac558c
Emir Pesereklü <emir.peserekli@gmail.com>
2025-04-18 23:23:23 +0330

    PremiumBehavior class is fully set

---
+++ src/behavior/PremiumBehavior.java
@@ -0,0 +1,28 @@
+package behavior;
+
+import music.Music;
+import music.Playlist;
+
+public class PremiumBehavior implements UserBehavior {
+    int month;
+
+    public PremiumBehavior(int month) {
+        this.month = month;
+    }
+
+    @Override
+    public void createPlaylist(String title, User owner) {
+        Playlist playlist = new Playlist(title, owner);
+        owner.playlists.add(playlist);
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        music.play();
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+        this.month += month;
+    }
+}
